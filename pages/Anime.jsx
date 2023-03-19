1a9b3885b05e098f5c944e346a1e382faf6fa4d4
Shivang <Shivangkh26@gmail.com>
2023-03-19 23:09:42 +0530

    head element added to pages

--- pages/Anime.jsx
+++ pages/Anime.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import React, { useState, useEffect } from "react";
 import {
   Button,
@@ -64,6 +65,14 @@ const Anime = () => {
   const formatResult = (item) => {
     return (
       <>
+       <Head>
+        <title>🎬 Anime Recommendations</title>
+        <meta name="description" content="Anime and Movies Recommendation system and can also be used for recommendations based on multiple anime or movies input. It is a static website and user can use this website to get recommendations for their next Anime to watch or next Movie to watch." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+      </Head>
+
+
         <div css={{ display: "flex" }}>
           <span style={{ display: "flex", textAlign: "left" }}>
             {item.English}&nbsp;({item.Type})
